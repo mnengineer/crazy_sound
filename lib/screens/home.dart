9781c419997e185861fe185810523b3b222d74a6
mnengineer <masashiii0115@gmail.com>
2023-08-14 15:56:50 +0900

    feat: アプリのベース作成

---
+++ lib/screens/home.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final audioPlayer = AudioPlayer();
+
+  final List<String> _listItem = [
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+    'assets/images/baby.jpg',
+  ];
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[600],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        // gooodbutton配置：レビューへの動線
+        leading: const Icon(Icons.menu),
+        title: const Text("Strange Sounds"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: 36,
+              height: 30,
+              decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Center(child: Text("0")),
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                        image: AssetImage('assets/images/baby.jpg'),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient:
+                          LinearGradient(begin: Alignment.bottomRight, colors: [
+                        Colors.black.withOpacity(.4),
+                        Colors.black.withOpacity(.2),
+                      ])),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "Lifestyle Sale",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(horizontal: 40),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Center(
+                            child: Text(
+                          "Shop Now",
+                          style: TextStyle(
+                              color: Colors.grey[900],
+                              fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                  child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: _listItem
+                    .map((item) => Card(
+                          color: Colors.transparent,
+                          elevation: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                    image: AssetImage(item),
+                                    fit: BoxFit.cover)),
+                            child: Transform.translate(
+                              offset: const Offset(50, -50),
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 60),
+                                width: 40,
+                                height: 40,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    shape: const CircleBorder(),
+                                    primary: Colors.grey[800],
+                                  ),
+                                  onPressed: () {
+                                    audioPlayer.play(
+                                      AssetSource('videos/baby.mp3'),
+                                    );
+                                  },
+                                  child: const Icon(Icons.play_arrow),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
