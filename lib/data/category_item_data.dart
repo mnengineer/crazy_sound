be5e599d455509d49cafb0765a73e9148ea62e62
mnengineer <masashiii0115@gmail.com>
2023-08-17 16:32:42 +0900

    feat: カテゴリアイテムのデータファイル作成

---
+++ lib/data/category_item_data.dart
@@ -0,0 +1,49 @@
+import 'package:strange_sounds/models/category_item.dart';
+
+const categoryItemData = [
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+  CategoryItem(
+    title: 'Animal',
+    imageUrl: 'assets/images/baby.jpg',
+    audioUrl: 'assets/videos/baby.mp3',
+  ),
+];
---
+++ lib/models/category_item.dart
@@ -0,0 +1,11 @@
+class CategoryItem {
+  const CategoryItem({
+    required this.title,
+    required this.imageUrl,
+    required this.audioUrl,
+  });
+
+  final String title;
+  final String imageUrl;
+  final String audioUrl;
+}
