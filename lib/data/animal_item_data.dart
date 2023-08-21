0d77fe962dd3ee1af829b79082ab51e292366c70
mnengineer <masashiii0115@gmail.com>
2023-08-21 21:45:08 +0900

    feat: AnimalData反映

--- lib/data/animal_item_data.dart
+++ lib/data/animal_item_data.dart
@@ -2,14 +2,39 @@ import 'package:strange_sounds/models/category_item.dart';
 
 const animalItemData = [
   CategoryItem(
+    title: 'alligator',
+    imageUrl: 'assets/images/animal/alligator.png',
+    audioUrl: 'assets/videos/animal/alligator.mp3',
+  ),
+  CategoryItem(
+    title: 'alpaca',
+    imageUrl: 'assets/images/animal/alpaca.png',
+    audioUrl: 'assets/videos/animal/alpaca.mp3',
+  ),
+  CategoryItem(
+    title: 'anteater',
+    imageUrl: 'assets/images/animal/anteater.png',
+    audioUrl: 'assets/videos/animal/anteater.mp3',
+  ),
+  CategoryItem(
+    title: 'antelope',
+    imageUrl: 'assets/images/animal/antelope.png',
+    audioUrl: 'assets/videos/animal/antelope.mp3',
+  ),
+  CategoryItem(
+    title: 'ape',
+    imageUrl: 'assets/images/animal/ape.png',
+    audioUrl: 'assets/videos/animal/ape.mp3',
+  ),
+  CategoryItem(
+    title: 'camel',
+    imageUrl: 'assets/images/animal/camel.png',
+    audioUrl: 'assets/videos/animal/camel.mp3',
+  ),
+  CategoryItem(
     title: 'cat',
     imageUrl: 'assets/images/animal/cat.png',
     audioUrl: 'assets/videos/animal/cat.mp3',
-  ),
-  CategoryItem(
-    title: 'dog',
-    imageUrl: 'assets/images/animal/dog.png',
-    audioUrl: 'assets/videos/animal/dog.mp3',
   ),
   CategoryItem(
     title: 'cow',
@@ -17,9 +42,14 @@ const animalItemData = [
     audioUrl: 'assets/videos/animal/cow.mp3',
   ),
   CategoryItem(
-    title: 'donkey',
-    imageUrl: 'assets/images/animal/donkey.png',
-    audioUrl: 'assets/videos/animal/donkey.mp3',
+    title: 'dinosaur',
+    imageUrl: 'assets/images/animal/dinosaur.png',
+    audioUrl: 'assets/videos/animal/dinosaur.mp3',
+  ),
+  CategoryItem(
+    title: 'dog',
+    imageUrl: 'assets/images/animal/dog.png',
+    audioUrl: 'assets/videos/animal/dog.mp3',
   ),
   CategoryItem(
     title: 'elephant',
@@ -27,9 +57,34 @@ const animalItemData = [
     audioUrl: 'assets/videos/animal/elephant.mp3',
   ),
   CategoryItem(
-    title: 'goat',
-    imageUrl: 'assets/images/animal/goat.png',
-    audioUrl: 'assets/videos/animal/goat.mp3',
+    title: 'ferret',
+    imageUrl: 'assets/images/animal/ferret.png',
+    audioUrl: 'assets/videos/animal/ferret.mp3',
+  ),
+  CategoryItem(
+    title: 'frog',
+    imageUrl: 'assets/images/animal/frog.png',
+    audioUrl: 'assets/videos/animal/frog.mp3',
+  ),
+  CategoryItem(
+    title: 'giraffe',
+    imageUrl: 'assets/images/animal/giraffe.png',
+    audioUrl: 'assets/videos/animal/giraffe.mp3',
+  ),
+  CategoryItem(
+    title: 'guinea-pig',
+    imageUrl: 'assets/images/animal/guinea-pig.png',
+    audioUrl: 'assets/videos/animal/guinea-pig.mp3',
+  ),
+  CategoryItem(
+    title: 'hedgehog',
+    imageUrl: 'assets/images/animal/hedgehog.png',
+    audioUrl: 'assets/videos/animal/hedgehog.mp3',
+  ),
+  CategoryItem(
+    title: 'hippopotamus',
+    imageUrl: 'assets/images/animal/hippopotamus.png',
+    audioUrl: 'assets/videos/animal/hippopotamus.mp3',
   ),
   CategoryItem(
     title: 'horse',
@@ -37,9 +92,44 @@ const animalItemData = [
     audioUrl: 'assets/videos/animal/horse.mp3',
   ),
   CategoryItem(
-    title: 'monkey',
-    imageUrl: 'assets/images/animal/ape.png',
-    audioUrl: 'assets/videos/animal/monkey.mp3',
+    title: 'hyena',
+    imageUrl: 'assets/images/animal/hyena.png',
+    audioUrl: 'assets/videos/animal/hyena.mp3',
+  ),
+  CategoryItem(
+    title: 'komodo-dragon',
+    imageUrl: 'assets/images/animal/komodo-dragon.png',
+    audioUrl: 'assets/videos/animal/komodo-dragon.mp3',
+  ),
+  CategoryItem(
+    title: 'leopard',
+    imageUrl: 'assets/images/animal/leopard.png',
+    audioUrl: 'assets/videos/animal/leopard.mp3',
+  ),
+  CategoryItem(
+    title: 'lion',
+    imageUrl: 'assets/images/animal/lion.png',
+    audioUrl: 'assets/videos/animal/lion.mp3',
+  ),
+  CategoryItem(
+    title: 'lizard',
+    imageUrl: 'assets/images/animal/lizard.png',
+    audioUrl: 'assets/videos/animal/lizard.mp3',
+  ),
+  CategoryItem(
+    title: 'moose',
+    imageUrl: 'assets/images/animal/moose.png',
+    audioUrl: 'assets/videos/animal/moose.mp3',
+  ),
+  CategoryItem(
+    title: 'otter',
+    imageUrl: 'assets/images/animal/otter.png',
+    audioUrl: 'assets/videos/animal/otter.mp3',
+  ),
+  CategoryItem(
+    title: 'panda',
+    imageUrl: 'assets/images/animal/panda.png',
+    audioUrl: 'assets/videos/animal/panda.mp3',
   ),
   CategoryItem(
     title: 'pig',
@@ -47,13 +137,73 @@ const animalItemData = [
     audioUrl: 'assets/videos/animal/pig.mp3',
   ),
   CategoryItem(
+    title: 'rabbit',
+    imageUrl: 'assets/images/animal/rabbit.png',
+    audioUrl: 'assets/videos/animal/rabbit.mp3',
+  ),
+  CategoryItem(
+    title: 'raccoon',
+    imageUrl: 'assets/images/animal/raccoon.png',
+    audioUrl: 'assets/videos/animal/raccoon.mp3',
+  ),
+  CategoryItem(
+    title: 'rat',
+    imageUrl: 'assets/images/animal/rat.png',
+    audioUrl: 'assets/videos/animal/rat.mp3',
+  ),
+  CategoryItem(
+    title: 'rattlesnake',
+    imageUrl: 'assets/images/animal/rattlesnake.png',
+    audioUrl: 'assets/videos/animal/rattlesnake.mp3',
+  ),
+  CategoryItem(
+    title: 'rhinoceros',
+    imageUrl: 'assets/images/animal/rhinoceros.png',
+    audioUrl: 'assets/videos/animal/rhinoceros.mp3',
+  ),
+  CategoryItem(
+    title: 'scorpion',
+    imageUrl: 'assets/images/animal/scorpion.png',
+    audioUrl: 'assets/videos/animal/scorpion.mp3',
+  ),
+  CategoryItem(
+    title: 'shark',
+    imageUrl: 'assets/images/animal/shark.png',
+    audioUrl: 'assets/videos/animal/shark.mp3',
+  ),
+  CategoryItem(
     title: 'sheep',
     imageUrl: 'assets/images/animal/sheep.png',
     audioUrl: 'assets/videos/animal/sheep.mp3',
   ),
   CategoryItem(
+    title: 'tiger',
+    imageUrl: 'assets/images/animal/tiger.png',
+    audioUrl: 'assets/videos/animal/tiger.mp3',
+  ),
+  CategoryItem(
+    title: 'turtle',
+    imageUrl: 'assets/images/animal/turtle.png',
+    audioUrl: 'assets/videos/animal/turtle.mp3',
+  ),
+  CategoryItem(
     title: 'whale',
     imageUrl: 'assets/images/animal/whale.png',
     audioUrl: 'assets/videos/animal/whale.mp3',
+  ),
+  CategoryItem(
+    title: 'wolf',
+    imageUrl: 'assets/images/animal/wolf.png',
+    audioUrl: 'assets/videos/animal/wolf.mp3',
+  ),
+  CategoryItem(
+    title: 'yak',
+    imageUrl: 'assets/images/animal/yak.png',
+    audioUrl: 'assets/videos/animal/yak.mp3',
+  ),
+  CategoryItem(
+    title: 'zebra',
+    imageUrl: 'assets/images/animal/zebra.png',
+    audioUrl: 'assets/videos/animal/zebra.mp3',
   ),
 ];
