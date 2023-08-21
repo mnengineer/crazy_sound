5d01a1708d97b9e1bf9e2bc89a7b9c5d4471e580
mnengineer <masashiii0115@gmail.com>
2023-08-21 18:27:15 +0900

    fix: ビルドエラー解消

---
+++ ios/Pods/Target Support Files/in_app_review/in_app_review-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_in_app_review : NSObject
+@end
+@implementation PodsDummy_in_app_review
+@end
--- lib/data/category_item_data.dart
+++ lib/data/category_item_data.dart
@@ -4,46 +4,6 @@ const categoryItemData = [
   CategoryItem(
     title: 'Animal',
     imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
-  ),
-  CategoryItem(
-    title: 'Animal',
-    imageUrl: 'assets/images/baby.jpg',
-    audioUrl: 'assets/videos/baby.mp3',
+    audioUrl: 'assets/videos/animal/cat.mp3',
   ),
 ];
