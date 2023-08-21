53a466b13e566dd6824a5360b3204d5566643c8f
mnengineer <masashiii0115@gmail.com>
2023-08-21 20:42:14 +0900

    feat: 画面の向き縦固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:strange_sounds/firebase_options.dart';
 import 'package:strange_sounds/screens/home.dart';
@@ -11,6 +12,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const App());
 }
 
